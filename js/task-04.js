591f302c9c70ce44acfd16ba794320466108fb35
Dima Samoylik <samoylikd@gmail.com>
2022-09-08 18:18:57 +0300

    Update task-04.js
    fix

--- js/task-04.js
+++ js/task-04.js
@@ -1,20 +1,20 @@
 let counter = 0;
-const decrement = document.querySelector("button[data-action=decrement]");
-const increment = document.querySelector("button[data-action=increment]");
-const value = document.querySelector("#value");
+const decrement = document.querySelector('button[data-action=decrement]');
+const increment = document.querySelector('button[data-action=increment]');
+const value = document.querySelector('#value');
 
 console.log(decrement);
 console.log(increment);
 
 const decr = function () {
-  counter += 1;
-  value.textContent = counter;
-};
-
-const incr = function () {
   counter -= 1;
   value.textContent = counter;
 };
 
-decrement.addEventListener("click", decr);
-increment.addEventListener("click", incr);
+const incr = function () {
+  counter += 1;
+  value.textContent = counter;
+};
+
+decrement.addEventListener('click', decr);
+increment.addEventListener('click', incr);
